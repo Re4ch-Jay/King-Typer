89118d0e899d0f32453aa143aca24ade260904f6
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-13 10:29:46 +0700

    adds: style to leaderboard link username

--- resources/js/Components/LeaderboardItem.jsx
+++ resources/js/Components/LeaderboardItem.jsx
@@ -6,7 +6,7 @@ function LeaderboardItem({ rank, username, wpm, accuracy }) {
         <div className="p-4 flex items-center justify-between">
             <div className="flex items-center">
                 <span className="text-xl text-slate-400 font-bold mr-4">{rank}</span>
-                <Link href={route('profile.index', username)} className="text-slate-100 text-xl">{username}</Link>
+                <Link href={route('profile.index', username)} className="text-slate-100 text-xl hover:text-slate-400">{username}</Link>
             </div>
             <div>
                 <span className="text-xl text-slate-100 font-bold mr-4">{wpm} WPM</span>
