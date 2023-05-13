de0292595d532ff1be7e3e164d784cd2bf0957a4
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-13 10:28:12 +0700

    adds: link to profile stats

--- resources/js/Components/LeaderboardItem.jsx
+++ resources/js/Components/LeaderboardItem.jsx
@@ -1,10 +1,12 @@
+import { Link } from "@inertiajs/react";
+
 // Components/LeaderboardItem.jsx
 function LeaderboardItem({ rank, username, wpm, accuracy }) {
     return (
         <div className="p-4 flex items-center justify-between">
             <div className="flex items-center">
                 <span className="text-xl text-slate-400 font-bold mr-4">{rank}</span>
-                <span className="text-slate-100 text-xl">{username}</span>
+                <Link href={route('profile.index', username)} className="text-slate-100 text-xl">{username}</Link>
             </div>
             <div>
                 <span className="text-xl text-slate-100 font-bold mr-4">{wpm} WPM</span>
