b6a3b129eb2e7116b871117e692427765ae7e185
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-19 22:08:23 +0700

    simplify useWord lib

--- resources/js/hooks/useWords.js
+++ resources/js/hooks/useWords.js
@@ -2,73 +2,44 @@ import { useCallback, useState } from "react";
 import { getRandomWord } from '../lib/wordGenerator';
 
 const generateWords = (count, option) => {
-  switch (option) {
-    case 'city':
-      return getRandomWord("City", count);
-    case 'name':
-      return getRandomWord("Name", count);
-    case 'email':
-      return getRandomWord("Email", count);
-    case 'animal':
-      return getRandomWord("Animal", count);
-    case 'commerce':
-      return getRandomWord("Commerce", count);
-    case 'company':
-      return getRandomWord("Company", count);
-    case 'lorem':
-      return getRandomWord("Lorem", count);
-    case 'music':
-      return getRandomWord("Music", count);
-    case 'vehicle':
-      return getRandomWord("Vehicle", count);
-    case 'system':
-      return getRandomWord("System", count);
-    case 'number':
-      return getRandomWord('Number', count);
-    case 'english':
-      return getRandomWord('English', count);
-    case 'spainish':
-      return getRandomWord('Spanish', count);
-    case 'french':
-      return getRandomWord('French', count);
-    case 'greek':
-      return getRandomWord('Greek', count);
-    case 'korean':
-      return getRandomWord('Korean', count);
-    case 'japanese':
-      return getRandomWord('Japanese', count);
-    case 'norwegian':
-      return getRandomWord('Norwegian', count);
-    case 'portuguese':
-      return getRandomWord('Portuguese', count);
-    case 'russian':
-      return getRandomWord('Russian', count);
-    case 'swedish':
-      return getRandomWord('Swedish', count);
-    case 'german':
-      return getRandomWord('German', count);
-    case 'ukrainian':
-      return getRandomWord('Ukrainian', count);
-    case 'lithuanian':
-      return getRandomWord('Lithuanian', count);
-    case 'italian':
-      return getRandomWord('Italian', count);
-    case 'filipino':
-      return getRandomWord('Filipino', count);
-    case 'estonian':
-      return getRandomWord('Estonian', count);
-    case 'czech':
-      return getRandomWord('Czech', count);
-    case 'armenian':
-      return getRandomWord('Armenian', count);
-    case 'khmer':
-      return getRandomWord('Khmer', count);
-    case 'arabic':
-      return getRandomWord('Arabic', count);
-    default:
-      return getRandomWord('English', count);
-  }
+  const wordOptions = {
+    'city': 'City',
+    'name': 'Name',
+    'email': 'Email',
+    'animal': 'Animal',
+    'commerce': 'Commerce',
+    'company': 'Company',
+    'lorem': 'Lorem',
+    'music': 'Music',
+    'vehicle': 'Vehicle',
+    'system': 'System',
+    'number': 'Number',
+    'english': 'English',
+    'spainish': 'Spanish',
+    'french': 'French',
+    'greek': 'Greek',
+    'korean': 'Korean',
+    'japanese': 'Japanese',
+    'norwegian': 'Norwegian',
+    'portuguese': 'Portuguese',
+    'russian': 'Russian',
+    'swedish': 'Swedish',
+    'german': 'German',
+    'ukrainian': 'Ukrainian',
+    'lithuanian': 'Lithuanian',
+    'italian': 'Italian',
+    'filipino': 'Filipino',
+    'estonian': 'Estonian',
+    'czech': 'Czech',
+    'armenian': 'Armenian',
+    'khmer': 'Khmer',
+    'arabic': 'Arabic',
+  };
+
+  const selectedOption = wordOptions[option] || 'English';
+  return getRandomWord(selectedOption, count);
 };
+
 
 const useWords = (count, option) => {
   const [words, setWords] = useState(generateWords(count, option));
