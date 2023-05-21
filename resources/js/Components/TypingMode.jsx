4264b16bc54f5fb00469cf354cf190009774bd7f
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-21 14:10:23 +0700

    simplify Typing Mode Components

--- resources/js/Components/TypingMode.jsx
+++ resources/js/Components/TypingMode.jsx
@@ -1,180 +1,153 @@
-import { useState } from "react"
+import React, { useState } from "react";
 import { TbWorld } from "react-icons/tb";
 import { BiTimeFive } from "react-icons/bi";
 import Card from "./Card";
-import React from "react";
-const TypingMode = ({ handleSeconds, handleNumberOfWords, handleWordTypes }) => {
+import { WordTypeModal } from "./WordTypeModal";
 
+const TypingMode = ({
+    handleSeconds,
+    handleNumberOfWords,
+    handleWordTypes,
+}) => {
     const [openModal, setOpenModal] = useState(false);
-
-    const [timeMode, setTimeMode] = useState(true)
-
-    const [wordMode, setWordMode] = useState(false)
+    const [timeMode, setTimeMode] = useState(true);
+    const [wordMode, setWordMode] = useState(false);
     const [languageModel, setLanguageModel] = useState(false);
-
-    const [customNumberOfWordsModal, setCustomNumberOfWordsModal] = useState(false)
-    const [customTimeModal, setCustomTimeModal] = useState(false)
-
+    const [customNumberOfWordsModal, setCustomNumberOfWordsModal] = useState(
+        false
+    );
+    const [customTimeModal, setCustomTimeModal] = useState(false);
     const [word, setWord] = useState(true);
-    const [number, setNumber] = useState(false)
+    const [number, setNumber] = useState(false);
+
+    const handleClickWord = (type) => {
+        handleWordTypes(type);
+        setWord(true);
+        setNumber(false);
+    };
+
+    const handleClickNumber = (type) => {
+        handleWordTypes(type);
+        setNumber(true);
+        setWord(false);
+    };
+
+    const handleClickTimeMode = () => {
+        setTimeMode(true);
+        setWordMode(false);
+    };
+
+    const handleClickWordMode = () => {
+        setWordMode(true);
+        setTimeMode(false);
+    };
 
     return (
-        <Card className='mb-10'>
-
-            {word ? (
-                <div onClick={() => { handleWordTypes('english'); setWord(true); setNumber(false); }} className="text-primary-500 hover:text-gray-800 cursor-pointer"><strong>Aa</strong> Words</div>
-            ) : (
-                <div onClick={() => { handleWordTypes('english'); setWord(true); setNumber(false); }} className="text-slate-500 hover:text-gray-800 cursor-pointer"><strong>Aa Words</strong></div>
-            )}
-            {number ? (
-                <div onClick={() => { handleWordTypes('number'); setNumber(true); setWord(false); }} className="text-primary-500 hover:text-gray-800 cursor-pointer"># Number</div>
-            ) : (
-                <div onClick={() => { handleWordTypes('number'); setNumber(true); setWord(false); }} className="text-slate-500 hover:text-gray-800 cursor-pointer"># Number</div>
-            )}
-
+        <Card className="mb-10">
+            <div
+                onClick={() => handleClickWord("english")}
+                className={`text-${word ? "primary" : "slate"}-500 hover:text-gray-800 cursor-pointer`}
+            >
+                <strong>Aa</strong> Words
+            </div>
+            <div
+                onClick={() => handleClickNumber("number")}
+                className={`text-${number ? "primary" : "slate"}-500 hover:text-gray-800 cursor-pointer`}
+            >
+                # Number
+            </div>
 
             <div
                 onClick={() => setLanguageModel(!languageModel)}
-                className="text-slate-500 hover:text-gray-800 cursor-pointer">
+                className="text-slate-500 hover:text-gray-800 cursor-pointer"
+            >
                 <TbWorld />
             </div>
-            <button onClick={() => setOpenModal(!openModal)} className="text-slate-500 hover:text-gray-800 cursor-pointer">...</button>
+            <button
+                onClick={() => setOpenModal(!openModal)}
+                className="text-slate-500 hover:text-gray-800 cursor-pointer"
+            >
+                ...
+            </button>
             <div className="bg-gray-800 p-1"></div>
 
-            {timeMode ? (
-                <button
-                    onClick={() => { setTimeMode(true); setWordMode(false) }}
-                    className="text-primary-500 hover:text-gray-800 cursor-pointer flex justify-center items-center">
-                    Time
-                    <BiTimeFive />
-                </button>
-            ) : (
-                <button
-                    onClick={() => { setTimeMode(true); setWordMode(false) }}
-                    className="text-slate-500 hover:text-gray-800 cursor-pointer flex justify-center items-center">
-                    Time <BiTimeFive />
-                </button>
-            )}
+            <button
+                onClick={handleClickTimeMode}
+                className={`text-${timeMode ? "primary" : "slate"}-500 hover:text-gray-800 cursor-pointer flex justify-center items-center`}
+            >
+                Time <BiTimeFive />
+            </button>
 
-            {wordMode ? (
-                <button
-                    onClick={() => { setWordMode(true); setTimeMode(false) }}
-                    className="text-primary-500 hover:text-gray-800 cursor-pointer">
-                    Words Count
-
-                </button>
-            ) : (
-                <button
-                    onClick={() => { setWordMode(true); setTimeMode(false) }}
-                    className="text-slate-500 hover:text-gray-800 cursor-pointer">
-                    Words Count
-
-                </button>
-            )}
+            <button
+                onClick={handleClickWordMode}
+                className={`text-${wordMode ? "primary" : "slate"}-500 hover:text-gray-800 cursor-pointer`}
+            >
+                Words Count
+            </button>
 
             <div className="bg-gray-800 p-1"></div>
 
             {wordMode && (
                 <>
-                    <button onClick={() => handleNumberOfWords(15)} className="text-slate-500 hover:text-gray-800 cursor-pointer">15</button>
-                    <button onClick={() => handleNumberOfWords(30)} className="text-slate-500 hover:text-gray-800 cursor-pointer">30</button>
-                    <button onClick={() => handleNumberOfWords(120)} className="text-slate-500 hover:text-gray-800 cursor-pointer">120</button>
+                    <button
+                        onClick={() => handleNumberOfWords(15)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        15
+                    </button>
+                    <button
+                        onClick={() => handleNumberOfWords(30)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        30
+                    </button>
+                    <button
+                        onClick={() => handleNumberOfWords(120)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        120
+                    </button>
                 </>
             )}
 
             {timeMode && (
                 <>
-                    <button onClick={() => handleSeconds(15)} className="text-slate-500 hover:text-gray-800 cursor-pointer">15s</button>
-                    <button onClick={() => handleSeconds(30)} className="text-slate-500 hover:text-gray-800 cursor-pointer">30s</button>
-                    <button onClick={() => handleSeconds(60)} className="text-slate-500 hover:text-gray-800 cursor-pointer">60s</button>
+                    <button
+                        onClick={() => handleSeconds(15)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        15s
+                    </button>
+                    <button
+                        onClick={() => handleSeconds(30)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        30s
+                    </button>
+                    <button
+                        onClick={() => handleSeconds(60)}
+                        className="text-slate-500 hover:text-gray-800 cursor-pointer"
+                    >
+                        60s
+                    </button>
                 </>
             )}
 
-            {openModal && (
-                <WordTypeModal setOpenModal={setOpenModal} handleWordTypes={handleWordTypes} />
-            )}
+            {openModal && <WordTypeModal setOpenModal={setOpenModal} handleWordTypes={handleWordTypes} />}
 
             {languageModel && (
                 <LanguageTypeModal setLanguageModel={setLanguageModel} handleWordTypes={handleWordTypes} />
             )}
 
-            {customNumberOfWordsModal && (
-                <CusomNumberWord setCustomNumberOfWordsModal={setCustomNumberOfWordsModal} />
-            )}
+            {customNumberOfWordsModal && <CusomNumberWord setCustomNumberOfWordsModal={setCustomNumberOfWordsModal} />}
 
-            {customTimeModal && (
-                <CustomTimeModal setCustomTimeModal={setCustomTimeModal} />
-            )}
-
+            {customTimeModal && <CustomTimeModal setCustomTimeModal={setCustomTimeModal} />}
         </Card>
+    );
+};
 
-    )
-}
+export default TypingMode;
 
-export default TypingMode
-
-const WordTypeModal = ({ setOpenModal, handleWordTypes }) => {
-
-    const options = [
-        { range: 1, displays: "city" },
-        { range: 2, displays: "name" },
-        { range: 3, displays: "animal" },
-        { range: 4, displays: "commerce" },
-        { range: 5, displays: "company" },
-        { range: 6, displays: "lorem" },
-        { range: 7, displays: "music" },
-        { range: 8, displays: "vehicle" },
-        { range: 9, displays: "system" },
-        { range: 10, displays: "cityname" },
-        { range: 11, displays: "c" },
-        { range: 12, displays: "cpp" },
-        { range: 13, displays: "csharp" },
-        { range: 14, displays: "dart" },
-        { range: 15, displays: "html" },
-        { range: 16, displays: "java" },
-        { range: 17, displays: "javascript" },
-        { range: 18, displays: "kotlin" },
-        { range: 19, displays: "matlap" },
-        { range: 20, displays: "php" },
-        { range: 21, displays: "python" },
-        { range: 22, displays: "ruby" },
-        { range: 23, displays: "rust" },
-        { range: 24, displays: "sql" },
-        { range: 25, displays: "swift" },
-        { range: 26, displays: "typescript" },
-        { range: 27, displays: "englishQuote" },
-        { range: 28, displays: "frenchQuote" },
-        { range: 29, displays: "germanQuote" },
-        { range: 30, displays: "greekQuote" },
-        { range: 31, displays: "japaneseQuote" },
-        { range: 32, displays: "khmerQuote" },
-        { range: 33, displays: "koreanQuote" },
-        { range: 34, displays: "russianQuote" },
-        { range: 35, displays: "spanishQuote" },
-        { range: 36, displays: "swedishQuote" }
-    ];
-
-    return (
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div
-                className="fixed inset-0 w-full h-full bg-black opacity-40"
-                onClick={() => setOpenModal(false)}
-            ></div>
-            <div className="flex items-center min-h-screen px-4 py-8">
-                <div className="relative w-full max-w-lg p-4 mx-auto bg-slate-800  rounded-md shadow-lg">
-                    {options.map((range) => (
-                        <div
-                            key={range.range}
-                            onClick={() => { setOpenModal(false); handleWordTypes(range.displays) }}
-                            className="text-slate-500 hover:text-white cursor-pointer" >
-                            {range.displays.toLocaleUpperCase()}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
 
 const LanguageTypeModal = ({ setLanguageModel, handleWordTypes }) => {
     const languageOptions = [
@@ -244,7 +217,6 @@ function CusomNumberWord({ setCustomNumberOfWordsModal }) {
     )
 }
 
-
 function CustomTimeModal({ setCustomTimeModal }) {
     return (
         <Modal onClick={() => setCustomTimeModal(false)}>
@@ -257,7 +229,6 @@ function CustomTimeModal({ setCustomTimeModal }) {
         </Modal>
     )
 }
-
 
 function Modal({ children, onClick }) {
     return (
---
+++ resources/js/Components/WordTypeModal.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+export const WordTypeModal = ({ setOpenModal, handleWordTypes }) => {
+
+    const options = [
+        { range: 1, displays: "city" },
+        { range: 2, displays: "name" },
+        { range: 3, displays: "animal" },
+        { range: 4, displays: "commerce" },
+        { range: 5, displays: "company" },
+        { range: 6, displays: "lorem" },
+        { range: 7, displays: "music" },
+        { range: 8, displays: "vehicle" },
+        { range: 9, displays: "system" },
+        { range: 10, displays: "cityname" },
+        { range: 11, displays: "c" },
+        { range: 12, displays: "cpp" },
+        { range: 13, displays: "csharp" },
+        { range: 14, displays: "dart" },
+        { range: 15, displays: "html" },
+        { range: 16, displays: "java" },
+        { range: 17, displays: "javascript" },
+        { range: 18, displays: "kotlin" },
+        { range: 19, displays: "matlap" },
+        { range: 20, displays: "php" },
+        { range: 21, displays: "python" },
+        { range: 22, displays: "ruby" },
+        { range: 23, displays: "rust" },
+        { range: 24, displays: "sql" },
+        { range: 25, displays: "swift" },
+        { range: 26, displays: "typescript" },
+        { range: 27, displays: "englishQuote" },
+        { range: 28, displays: "frenchQuote" },
+        { range: 29, displays: "germanQuote" },
+        { range: 30, displays: "greekQuote" },
+        { range: 31, displays: "japaneseQuote" },
+        { range: 32, displays: "khmerQuote" },
+        { range: 33, displays: "koreanQuote" },
+        { range: 34, displays: "russianQuote" },
+        { range: 35, displays: "spanishQuote" },
+        { range: 36, displays: "swedishQuote" }
+    ];
+
+    return (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div
+                className="fixed inset-0 w-full h-full bg-black opacity-40"
+                onClick={() => setOpenModal(false)}
+            ></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+                <div className="relative w-full max-w-lg p-4 mx-auto bg-slate-800  rounded-md shadow-lg">
+                    {options.map((range) => (
+                        <div
+                            key={range.range}
+                            onClick={() => { setOpenModal(false); handleWordTypes(range.displays) }}
+                            className="text-slate-500 hover:text-white cursor-pointer" >
+                            {range.displays.toLocaleUpperCase()}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
