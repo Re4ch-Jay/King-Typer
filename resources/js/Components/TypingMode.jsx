d0e6c0c31bffefc7f87fe362454a97c91ad78e55
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-21 14:11:27 +0700

    extract LanguageTypeModal

--- resources/js/Components/TypingMode.jsx
+++ resources/js/Components/TypingMode.jsx
@@ -3,6 +3,7 @@ import { TbWorld } from "react-icons/tb";
 import { BiTimeFive } from "react-icons/bi";
 import Card from "./Card";
 import { WordTypeModal } from "./WordTypeModal";
+import { LanguageTypeModal } from "./LanguageTypeModal";
 
 const TypingMode = ({
     handleSeconds,
@@ -147,54 +148,6 @@ const TypingMode = ({
 };
 
 export default TypingMode;
-
-
-const LanguageTypeModal = ({ setLanguageModel, handleWordTypes }) => {
-    const languageOptions = [
-        { range: 1, displays: "english" },
-        { range: 2, displays: "spainish" },
-        { range: 3, displays: "french" },
-        { range: 4, displays: "indian eng" },
-        { range: 5, displays: "italian" },
-        { range: 6, displays: "arabic" },
-        { range: 7, displays: "greek" },
-        { range: 8, displays: "korean" },
-        { range: 9, displays: "japanese" },
-        { range: 10, displays: "norwegian" },
-        { range: 11, displays: "portuguese" },
-        { range: 12, displays: "swedish" },
-        { range: 13, displays: "german" },
-        { range: 14, displays: "ukrainian" },
-        { range: 15, displays: "lithuanian" },
-        { range: 16, displays: "filipino" },
-        { range: 17, displays: "estonian" },
-        { range: 18, displays: "czech" },
-        { range: 19, displays: "armenian" },
-        { range: 20, displays: "russian" },
-        { range: 21, displays: "khmer" },
-    ];
-
-    return (
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div
-                className="fixed inset-0 w-full h-full bg-black opacity-40"
-                onClick={() => setLanguageModel(false)}
-            ></div>
-            <div className="flex items-center min-h-screen px-4 py-8">
-                <div className="relative w-full max-w-lg p-4 mx-auto bg-slate-800  rounded-md shadow-lg">
-                    {languageOptions.map((range) => (
-                        <div
-                            key={range.range}
-                            onClick={() => { setLanguageModel(false); handleWordTypes(range.displays) }}
-                            className="text-slate-500 hover:text-white cursor-pointer" >
-                            {range.displays.toLocaleUpperCase()}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
 
 function CusomNumberWord({ setCustomNumberOfWordsModal }) {
     return (
