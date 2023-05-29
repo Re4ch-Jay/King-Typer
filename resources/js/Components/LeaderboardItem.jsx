b0d9a9187af2a9c94151809ac7b3e2e0e6bcf31b
unknown <panhareach.phat@student.cadt.edu.kh>
2023-05-29 19:41:22 +0700

    adds animation to leaderboard

--- resources/js/Components/LeaderboardItem.jsx
+++ resources/js/Components/LeaderboardItem.jsx
@@ -1,9 +1,14 @@
 import { Link } from "@inertiajs/react";
-
+import { motion } from "framer-motion";
 // Components/LeaderboardItem.jsx
 function LeaderboardItem({ rank, username, wpm, accuracy, country }) {
+    const initial = { opacity: 0 };
+    const animate = { opacity: 1 };
     return (
-        <div className="p-4 flex items-center justify-between">
+        <motion.div
+            initial={initial}
+            animate={animate}
+            className="p-4 flex items-center justify-between">
             <div className="flex items-center">
                 <span className="text-xl text-slate-400 font-bold mr-4">{rank}</span>
                 <Link href={route('profile.index', username)} className="text-slate-100 text-xl hover:text-slate-400">{username} {country}</Link>
@@ -12,7 +17,7 @@ function LeaderboardItem({ rank, username, wpm, accuracy, country }) {
                 <span className="text-xl text-slate-100 font-bold mr-4">{wpm} WPM</span>
                 <span className="text-xl text-slate-100 font-bold">{accuracy} Accuracy</span>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
